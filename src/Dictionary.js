8f4af577941f7251779add6233760926af14d62c
KateRiinaO <orrava.kate@gmail.com>
2022-04-25 11:22:20 +0300

    making form alert input value

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 export default function Dictionary() {
+  let [word, setWord] = useState("");
+
   function search(event) {
     event.preventDefault();
-    alert("searching");
+    alert(`searching for ${word} value`);
+  }
+
+  function handleWordChange(event) {
+    console.log(event.target.value);
+    setWord(event.target.value);
   }
 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} />
+        <input type="search" autoFocus={true} onChange={handleWordChange} />
         <input type="submit" value="SEARCH" />
       </form>
     </div>
