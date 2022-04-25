654116667c49ea111a9cec9dc75719c7b0bb6297
KateRiinaO <orrava.kate@gmail.com>
2022-04-25 10:53:53 +0300

    Adding footer with coder, github repository link and netlify link

--- src/App.js
+++ src/App.js
@@ -4,10 +4,40 @@ import logo from "./logo.png";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>DICTIONARY</h1>
-        <img src={logo} alt="logo-katerina-orrava" className="logo img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>DICTIONARY</h1>
+          <img
+            src={logo}
+            alt="logo-katerina-orrava"
+            className="logo img-fluid"
+          />
+        </header>
+        <footer>
+          <small className="footer">
+            This project was coded by Katerīna Orrava and is open-sourced on{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/KateRiinaO/app-dictionary-react"
+              className="github-link"
+            >
+              {" "}
+              GitHub{" "}
+            </a>
+            and hosted on{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://leafy-puppy-a66235.netlify.app"
+              className="netlify-link"
+            >
+              {" "}
+              Netlify
+            </a>{" "}
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
